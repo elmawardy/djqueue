c2c37c39af0ffde06a16625e9885dfbab04fed76
Amr El Mawardy <amr.elmawardy94@yahoo.com>
2019-06-08 20:35:48 +0200

    profile multilingual 0

--- react-kitchen/src/index.js
+++ react-kitchen/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './HomePage.js'
+import App from './profile.js'
 
 ReactDOM.render( <App ref={(App) => {window.homeApp = App}} />, document.getElementById('root') );
